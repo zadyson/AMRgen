2c4b5323b1709314b29fb7b5b5132090bf5acf19
Kat Holt <drkatholt@gmail.com>
2025-01-14 14:42:28 +0000

    add gtdb import

---
+++ R/gtdb.R
@@ -0,0 +1,37 @@
+require(AMR)
+require(tidyverse)
+
+#' Get Microorganism from GTDB Species Name
+#' 
+#' Parse a character vector containing species names from GTDB output to get a valid microorganism code.
+#' 
+#' @param species
+#' 
+#' @return microorganism
+#' @export
+#' 
+#' @examples gtdb.mo("Escherichia_A coli_BC")
+gtdb.mo <- function(species) {
+  as.mo(species, cleaning_regex = paste0(mo_cleaning_regex(), "|_[A-Z]+($| )"), keep_synonyms = TRUE)
+}
+
+#' Import GTDB Output
+#' 
+#' Import GTDB output (from file or data frame) and parse the species name into a microorganism recognised by AMR package. AMR mo code and species name will be added to the data frame. 
+#'
+#' @param file File path to GTDB output file (tab-separated). If not given, tbl must be given.
+#' @param tbl Data frame containing GTDB output. If not given, file must be given.
+#' @param species_column Name of column containing the species call (default "Species").
+#'
+#' @return tbl Data frame containing GTDB output with AMR-parsed microorganism code (gtdb.mo) and species name (gtdb.species) appended.
+#' @export
+#'
+#' @examples import_gtdb(tbl = data.frame(Species = c("Pseudomonas_E piscis","Haemophilus_D parainfluenzae_A", "Acinetobacter calcoaceticus_C")))
+#' 
+import_gtdb <- function(file=NULL, tbl=NULL, species_column="Species") {
+  if(!is.null(file)) {
+    tbl <- read_tsv(file)
+  }
+  tbl %>% mutate(gtdb.mo=gtdb.mo(tbl[[species_column]]), gtdb.species=mo_name(gtdb.mo, keep_synonyms = TRUE))
+}
+
--- R/upset_plot.R
+++
@@ -1,9 +0,0 @@
-#' Create UpSet Plot
-#'
-#' Here the description.
-#' @param ... arguments passed on to function, currently unused
-#' @importFrom plot2 plot2
-#' @export
-upset_plot <- function(...) {
-
-}
