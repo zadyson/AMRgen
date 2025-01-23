23bf22b0f6b2aabdfdbdcb08fa2c6ad778bf2109
yekwah <jane.hawkey@monash.edu>
2025-01-23 15:51:10 +0000

    helper function to compare sample IDs

---
+++ R/helpers.R
@@ -0,0 +1,30 @@
+#input = pheno data frame, name of the ID column (otherwise take first column, or try to find 'sample' or 'biosample'; same for geno data frame
+#compare the sample IDs, make a list of unique entries that appear in both
+#report the number of overlaps and uniques
+#return copies of geno and pheno data frames that each contain the overlapping samples only
+
+compare_geno_pheno_id <- function(geno_data, pheno_data, geno_sample_col=NULL, pheno_sample_col=NULL){
+  if (is.null(geno_sample_col)){
+    geno_sample_col <- colnames(geno_data)[1]
+  }
+  if(is.null(pheno_sample_col)){
+    pheno_sample_col <- colnames(pheno_data)[1]
+  }
+
+  # get sample ids from both files, extract as vector
+  geno_sample_ids <- unique(pull(geno_data, geno_sample_col))
+  pheno_sample_ids <- unique(pull(pheno_data, pheno_sample_col))
+
+  # compare the two 
+  # Unique values in pheno and geno
+  unique_in_pheno <- setdiff(pheno_sample_ids, geno_sample_ids)
+  unique_in_geno <- setdiff(geno_sample_ids, pheno_sample_ids)
+
+  # Overlapping values
+  overlapping_values <- intersect(pheno_sample_ids, geno_sample_ids)
+
+  return(list(pheno_unique = unique_in_pheno, 
+    geno_unique = unique_in_geno, 
+    overlap_ids = overlapping_values))
+
+}
