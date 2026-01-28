0e9ebceee31fb41df4b83cbeb66af20287c86fba
Zoe Anne Dyson <zadyson@gmail.com>
2026-01-28 16:26:15 +0000

    Update import_ebi.R
    Updated to allow antibiotic selection

--- R/import_ebi.R
+++ R/import_ebi.R
@@ -34,6 +34,7 @@
 #'
 #' @param user_genus
 #' @param user_release
+#' @param user_antibiotic_name
 #'
 #' @return A data frame containing EBI genotype data
 #' @keywords internal
@@ -42,16 +43,16 @@
 #' \dontrun{
 #' import_ebi(
 #'     user_genus="Salmonella",
-#'     user_release="2025-12
+#'     user_release="2025-12",
+#'     user_antibiotic_name="ampicillin"
 #' )
 #' }
-import_ebi <- function(user_genus=NULL, user_release=NULL) {
+import_ebi <- function(user_genus=NULL, user_release=NULL, user_antibiotic_name=NULL) {
   
   # EBI source url
   ebi_url <- "ftp://ftp.ebi.ac.uk/pub/databases/amr_portal/releases/"
-  
+
   if (!is.null(user_release)){
-    
     ebi_geno <- getBinaryURL(print(gsub("ftp\\:", "https\\:", paste0(ebi_url,user_release,"/","genotype.parquet"))))
     ebi_geno <- read_parquet(ebi_geno)
     
@@ -59,13 +60,10 @@ import_ebi <- function(user_genus=NULL, user_release=NULL) {
   
     # get list of releases
     folders <- str_split(getURL(ebi_url, dirlistonly = TRUE), "\n")[[1]]
-    
     # get latest
-    latest_release <- folders %>% 
-      discard(~ .x %in% c("releases.yml","")) %>% 
+    latest_release <- folders[!folders %in% c("releases.yml", "")] %>% 
       max()
-    
-    ebi_geno <- getBinaryURL(print(gsub("ftp\\:", "https\\:", paste0(ebi_url,latest_release,"/","genotype.parquet"))))
+    ebi_geno <- getBinaryURL(gsub("ftp\\:", "https\\:", paste0(ebi_url,latest_release,"/","genotype.parquet")))
     ebi_geno <- read_parquet(ebi_geno)
     
   }
@@ -76,9 +74,12 @@ import_ebi <- function(user_genus=NULL, user_release=NULL) {
       filter(genus==user_genus)
   }
   
+  # filter by genus if required
+  if (!is.null(user_antibiotic_name)){
+    ebi_geno <- ebi_geno %>% 
+      filter(antibiotic_name==user_antibiotic_name)
+  }
+  
   # return data frame
   return(ebi_geno)
 }
-
-
-
